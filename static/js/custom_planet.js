c991e62ead8869ef059436e9a6ffa35e3add01e2
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2023-08-14 16:43:58 +0200

    [SOL1618] Pictures added. Focus on planet

--- static/js/custom_planet.js
+++ static/js/custom_planet.js
@@ -1,34 +1,42 @@
+const planetData = {
+    mercury: {
+        info: "Mercury is the smallest planet in our Solar System.",
+        imgPath: "/static/images/Mercury.png"
+    },
+    venus: {
+        info: "Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.",
+        imgPath: "/static/images/Venus.png"
+    },
+    // ... Add similar entries for other planets ...
+};
+
+
 document.getElementById('planet-search').addEventListener('input', function () {
     const planetName = this.value.toLowerCase();
-    const planets = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune'];
-    if (planets.includes(planetName)) {
-        // Display bubble info on the planet
-        const planetElement = document.querySelector(`.${planetName}`);
-        const existingBubble = planetElement.querySelector('.info-bubble');
-        if (!existingBubble) {
-            const infoBubble = document.createElement('div');
-            infoBubble.className = 'info-bubble';
-            infoBubble.innerText = planetName.charAt(0).toUpperCase() + planetName.slice(1);
-            planetElement.appendChild(infoBubble);
-        }
 
-        // Information about the planets
-        const planetInfo = {
-            mercury: "Mercury is the smallest planet in our Solar System.",
-            venus: "Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.",
-            // ... Add similar info for other planets ...
-        };
+    // Check if planet exists in our dataset
+    if (planetData[planetName]) {
+        // Get planet details from dataset
+        const { info, imgPath } = planetData[planetName];
 
         // Update Bootstrap card with planet information
         const planetCard = document.getElementById('planet-card');
         document.getElementById('planet-card-title').innerText = planetName.charAt(0).toUpperCase() + planetName.slice(1);
-        document.getElementById('planet-card-description').innerText = planetInfo[planetName] || "Information not available.";
-        planetCard.style.display = 'block'; // Display the card
+        document.getElementById('planet-card-description').innerText = info;
+        document.getElementById('planet-image').src = imgPath;
+        planetCard.style.display = 'block';
 
-        // Optionally, update the modal with detailed information too
+        // Update the modal with detailed information too
         document.getElementById('planetModalLabel').innerText = planetName.charAt(0).toUpperCase() + planetName.slice(1);
-        // For now, I'll populate the modal with the same basic info. You can expand on this:
-        document.querySelector('#planetModal .modal-body').innerText = planetInfo[planetName] || "Information not available.";
+        document.querySelector('#planetModal .modal-body').innerText = info;
+
+    } else {
+        // If planet doesn't exist in our dataset, show default "not found" message and image
+        const planetCard = document.getElementById('planet-card');
+        document.getElementById('planet-card-title').innerText = "Planet Not Found";
+        document.getElementById('planet-card-description').innerText = "The planet you're looking for could not be found.";
+        document.getElementById('planet-image').src = "/static/images/pluto.png"; // Default image path
+        planetCard.style.display = 'block';
     }
 });
 
